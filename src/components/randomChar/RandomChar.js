b3857ec07834144bf47a4055b218149682aaf62b
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-10-31 12:53:55 +0300

    corrected id in updeteChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -38,7 +38,7 @@ class RandomChar extends Component {
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
-            .getCharacter(3)
+            .getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
     }
