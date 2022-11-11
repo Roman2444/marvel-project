a19047f066f3db4e44183d6913a0413844ab6c63
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-11-11 20:04:12 +0300

    added spinner2

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Spinner from '../spinner/Spinner';
+import Spinner from '../spinner/Spinner2';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
