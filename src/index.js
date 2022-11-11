974fb413ad509d4fde55d8a13562665dbe8d85dd
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-11-11 16:37:56 +0300

    corrected import * as ReactDOMClient from react-dom/client in src/index.js, update package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/App';
 
 import './style/style.scss';
 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
