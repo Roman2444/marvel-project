076ebd8490c5e8b05048dd01b9117a0f2d1b17bf
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-11-12 11:14:35 +0300

    corrected import spinner

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner2';
+import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton'
 import './charInfo.scss';
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Spinner from '../spinner/Spinner2';
+import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
