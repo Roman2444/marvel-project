930a5dd923c0b95710dbb5ba0b31769caca2c820
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-11-22 00:07:40 +0300

    fixed http.hook.js

--- src/components/errorBoundary/ErrorBoundary.js
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import { Component }  from 'react';
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class ErrorBoundary extends Component {
--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,15 +1,15 @@
-import { useCallback, useState } from "react";
+import { useState, useCallback } from "react";
 
 export const useHttp = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const request = useCallback(async(url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
-        
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+
         setLoading(true);
 
         try {
-           const response = await fetch(url, {method, body, headers});
+            const response = await fetch(url, {method, body, headers});
 
             if (!response.ok) {
                 throw new Error(`Could not fetch ${url}, status: ${response.status}`);
@@ -19,16 +19,14 @@ export const useHttp = () => {
 
             setLoading(false);
             return data;
-
         } catch(e) {
             setLoading(false);
-            setError(e.messege);
+            setError(e.message);
             throw e;
         }
-
     }, []);
 
-    const clearError = useCallback(() => setError(null), [] )
+    const clearError = useCallback(() => setError(null), []);
 
     return {loading, request, error, clearError}
 }
